b348c02904b57aab23d1cafd2fce0dd5c4568a1d
prescott991 <149590373+prescott991@users.noreply.github.com>
2024-03-25 02:09:29 +0000

    Add files via upload

---
+++ resources/js/Components/ApplicationLogo.jsx
@@ -0,0 +1,7 @@
+import logo from '../Components/logo.svg'
+export default function ApplicationLogo(props) {
+    return (
+      <img src={logo} width='40'/>
+
+    );
+}
---
+++ resources/js/Components/SVGBackground.jsx
@@ -0,0 +1,17 @@
+// MySVGBackground.js
+import logo2 from '../Components/logo2.svg'
+import React from 'react';
+
+const MySVGBackground = () => {
+  return (
+    <style>
+
+
+      <img src={logo} width='40'/>
+
+    </style>
+
+  );
+};
+
+export default MySVGBackground;
